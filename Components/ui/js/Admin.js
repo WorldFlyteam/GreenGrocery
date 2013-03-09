3a917303efe1dce97e7c5bc3f33e45831a2cef6d
Sergey Khokhlov <worldfly@yandex.ru>
2013-03-09 16:04:33 +0400

    Update control panel

--- Components/ui/js/Admin.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-    $(".i-text-tiny").hover(function () {
-        var itexttiny = document.getElementsByClassName("i-text-tiny");
-        itexttiny.style.opacity = "1";
-    });   
-})
---
+++ Components/ui/js/All.js
@@ -0,0 +1,15 @@
+﻿
+// Generate a random string
+function randomString(length) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+
+    if (!length) {
+        length = Math.floor(Math.random() * chars.length);
+    }
+
+    var str = '';
+    for (var i = 0; i < length; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
